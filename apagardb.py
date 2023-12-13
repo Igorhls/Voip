7d00138f317d09d283ee204cc55324bb98a23ea4
igorhls <igorhls@hotmail.com>
2023-12-13 20:53:22 -0300

    função para apagar nome do bc

---
+++ apagardb.py
@@ -0,0 +1,18 @@
+from database_connection import *
+def apagar_dados_por_nome(nome):
+    # Função para apagar dados com base nos nomes
+    conn = conectar_banco()
+    cursor = conn.cursor()
+
+    consulta_sql = "DELETE FROM indicadores WHERE nome = %s"
+    dados_para_apagar = (nome,)
+
+    cursor.execute(consulta_sql, dados_para_apagar)
+    conn.commit()
+
+    print(f'Dados apagados com sucesso para o nome: {nome}')
+
+    cursor.close()
+    conn.close()
+
+apagar_dados_por_nome('Novo gama')
